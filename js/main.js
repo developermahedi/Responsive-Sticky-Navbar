a638cabb21e99778754dac3a7934b7d5bc2c3f71
mahedi hasan <mdmahedihasan727@gmail.com>
2021-07-04 23:42:16 +0600

    Sticky NavBar Design use html scss and js
    Responsive sticky Navbar

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// sticky navbar
+
+     window.addEventListener('scroll',function(){
+         const nav = document.querySelector('nav');
+         nav.classList.toggle('sticky', window.scrollY > 0);
+     });
